36004c48b5b7b74c57724d43ff00a9be28e4922c
Shvan <shvan.yakana@student.hu.nl>
2025-02-26 19:11:18 +0100

    feat capture player choice and store it

--- javascript.js
+++ javascript.js
@@ -3,7 +3,6 @@ console.log("hi");
 function getComputersChoice() {
   let cpuAnswer = Math.random();
   let finalAnswer = null;
-  console.log(cpuAnswer);
   if (cpuAnswer < 0.33) {
     finalAnswer = "rock";
   } else if (cpuAnswer > 0.33 && cpuAnswer < 0.66) {
@@ -12,4 +11,35 @@ function getComputersChoice() {
     finalAnswer = "scissors";
   }
   return finalAnswer;
+}
+
+function humanChoice() {
+  let userInput = prompt("User input:");
+  return userInput;
+}
+
+function game() {
+  let playerOne = humanChoice();
+  let computerChoice = getComputersChoice();
+  console.log(
+    `player's choice ${playerOne} computer's choice${computerChoice}`
+  );
+
+  if (playerOne === computerChoice) {
+    console.log("its a draw!!");
+  } else if (playerOne === "rock" && computerChoice === "paper") {
+    console.log("paper wins");
+  } else if (playerOne === "paper" && computerChoice === "rock") {
+    console.log("paper wins");
+  } else if (playerOne === "scissors" && computerChoice === "rock") {
+    console.log("rock! wins");
+  } else if (playerOne === "rock" && computerChoice === "scissors") {
+    console.log("rock wins");
+  } else if (playerOne === "paper" && computerChoice === "scissors") {
+    console.log("scissor wins");
+  } else if (playerOne === "scissors" && computerChoice === "paper") {
+    console.log("scissors wins");
+  } else {
+    console.log("invalid input");
+  }
 }
