1fb30c295c05c3e3f8371bea5ce9c1f31540a322
Shvan <shvan.yakana@student.hu.nl>
2025-02-26 10:52:46 +0100

    feat generate computer input(rock,paper,scissors)

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
 console.log("hi");
+
+function getComputersChoice() {
+  let cpuAnswer = Math.random();
+  let finalAnswer = null;
+  console.log(cpuAnswer);
+  if (cpuAnswer < 0.33) {
+    finalAnswer = "rock";
+  } else if (cpuAnswer > 0.33 && cpuAnswer < 0.66) {
+    finalAnswer = "paper";
+  } else {
+    finalAnswer = "scissors";
+  }
+  return finalAnswer;
+}
